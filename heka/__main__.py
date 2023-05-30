d4e018ce1effaa3ba393e5d761a5c1f9e197151a
AbooMinister25 <aboominister@gmail.com>
2023-05-30 14:34:20 -0400

    Add initial dependencies and hello world boilerplate

---
+++ heka/__main__.py
@@ -0,0 +1,10 @@
+"""Some module."""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Hello, World"
--- module_name/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Some module."""
-
-from __future__ import annotations
-
-print("Hello World")
